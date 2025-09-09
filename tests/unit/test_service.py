40d4d432a6bea1b63e0de7281e19bfe1ec304b51
Nils Ove Tendenes <nils.ove@tendenes.com>
2025-09-09 06:48:31 +0200

    Revert "fix: add include_empty=true to failing test"
    This reverts commit eee7e3543e03c6f4cb02f2a57fe96011fb19d29b.

--- tests/unit/test_service.py
+++ tests/unit/test_service.py
@@ -89,7 +89,7 @@ async def test_get_organization_catalogs_empty_response(
         mock_fetch_orgs.return_value = {}
 
         result = await org_catalog_service.get_organization_catalogs(
-            FilterEnum.NONE, "true"
+            FilterEnum.NONE, None
         )
 
         assert result is not None
